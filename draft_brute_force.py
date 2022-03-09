bd864dcc6ef816f553223f4de5397632b19de8bf
[William] <w.gavigniaux@gmail.com>
2022-03-09 11:05:25 +0100

    code clean-up

--- draft_brute_force.py
+++ draft_brute_force.py
@@ -21,38 +21,41 @@ actions = [
     ["action_19", 24, 21],
     ["action_20", 114, 18],
 ]
-actions_selected = []
-max_spendings = 500
-spendings = 0
-earnings = 0
 
-# Calculate gain for every action and add the data to the list
-for action in actions:
-    gain = action[1] * action[2] / 100
-    action.append(gain)
 
-# Sort the list of actions per highest rate first
-actions_sorted = sorted(actions, key=lambda x: x[2], reverse=True)
+def sort_best_actions(data, max_spendings):
+    actions_selected = []
+    spendings = 0
+    earnings = 0
 
-# For every action in the sorted list:
-# check total spendings have not reached spending cap: if yes stop the loop;
-# check action will not go over the max spending cap: if yes ignore it;
-# if no: retain this action.
-for action in actions_sorted:
-    if spendings == max_spendings:
-        break
-    elif (spendings + action[1]) > max_spendings:
-        continue
-    else:
-        spendings += action[1]
-        earnings += action[3]
-        actions_selected.append(action)
+    # Calculate gain for every action and add the data to the list
+    for action in data:
+        gain = action[1] * action[2] / 100
+        action.append(gain)
 
-# Show results:
-print("Total spendings :")
-print(spendings)
-print("Total earnings :")
-print(earnings)
-print("Actions selected :")
-for action in actions_selected:
-    print(action)
+    # Sort the list of actions per highest rate first
+    actions_sorted = sorted(data, key=lambda x: x[2], reverse=True)
+
+    # For every action in the sorted list:
+    # check total spendings have not reached spending cap: if yes stop the loop;
+    # check next action will not go over the max spending cap: if yes ignore it;
+    # else: retain this action.
+    for action in actions_sorted:
+        if spendings == max_spendings:
+            break
+        elif (spendings + action[1]) > max_spendings:
+            continue
+        else:
+            spendings += action[1]
+            earnings += action[3]
+            actions_selected.append(action)
+
+    # Show results:
+    print(f"Total spendings : {spendings}")
+    print(f"Total earnings : {earnings}")
+    print("Actions selected :")
+    for action in actions_selected:
+        print(action)
+
+
+sort_best_actions(actions, 500)
