7da287e37e50aebb141a5aabd0a29c1df1b21344
[William] <w.gavigniaux@gmail.com>
2022-03-09 18:36:42 +0100

    Added comments about algo and complexity; done some simplifications

--- greedy_algo.py
+++ greedy_algo.py
@@ -1,30 +1,31 @@
 import time
 
 
-def sort_best_actions(data, max_spendings):
+def greedy(data, max_spendings):
+    """
+    Greedy algorithm that chooses the best solution available at the time.
+    It isn't concerned about wether the current best outcome 
+    will provide the best overall result.
+    It is a top down approach.
+    Time complexity: O(NlogN) because of the sorting required.
+    Space complexity: O(1) because no additional memory required
+    """
     actions_selected = []
     spendings = 0
     earnings = 0
 
-    # Calculate gain for every action and add the data to the list
-    for action in data:
-        gain = action[1] * action[2] / 100
-        action.append(gain)
-
     # Sort the list of actions per highest rate first
     actions_sorted = sorted(data, key=lambda x: x[2], reverse=True)
-    print(actions_sorted)
 
     # For every action in the sorted list:
-    # check total spendings have not reached spending cap: if yes stop the loop;
-    # check next action will not go over the max spending cap: if yes ignore it;
-    # else: retain this action.
     for action in actions_sorted:
+        # check total spendings have not reached max spending cap
         if spendings == max_spendings:
             break
+        # check next action will not go over the max spending cap
         elif (spendings + action[1]) <= max_spendings:
             spendings += action[1]
-            earnings += action[3]
+            earnings += action[1] * action[2] / 100
             actions_selected.append(action)
 
     # Show results:
@@ -32,7 +33,7 @@ def sort_best_actions(data, max_spendings):
     print(f"Total earnings : {earnings}")
     print("Actions selected :")
     for action in actions_selected:
-        print(action)
+        print(action[0])
 
 
 def main():
@@ -62,8 +63,9 @@ def main():
         ["action_19", 24, 21],
         ["action_20", 114, 18],
     ]
+    max_spending = 500
 
-    sort_best_actions(actions, 500)
+    greedy(actions, max_spending)
 
     # Calculate runtime and display
     end = time.time()
