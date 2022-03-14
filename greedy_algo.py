2935ec4b5d019e03f0601732d5a63dd50fe7c985
Wil31 <80322222+Wil31@users.noreply.github.com>
2022-03-14 13:52:25 +0100

    Added explanations to greedy algo and cleaning

--- greedy_algo.py
+++ greedy_algo.py
@@ -1,14 +1,15 @@
 import time
+from config import ACTIONS, DATASET1, DATASET2, BUDGET
 
 
 def greedy(data, max_spendings):
     """
-    Greedy algorithm that chooses the best solution available at the time.
-    It isn't concerned about wether the current best outcome 
-    will provide the best overall result.
-    It is a top down approach.
+    Greedy algorithm that chooses the best solution available at the time,
+    and returns the max profit, total cost and list of selected actions.
+    It isn't concerned about wether the current best outcome will provide 
+    the best overall result. It is a top down approach.
     Time complexity: O(NlogN) because of the sorting required.
-    Space complexity: O(1) because no additional memory required
+    Space complexity: O(N) no additional memory required
     """
     actions_selected = []
     spendings = 0
@@ -19,55 +20,53 @@ def greedy(data, max_spendings):
 
     # For every action in the sorted list:
     for action in actions_sorted:
-        # check total spendings have not reached max spending cap
+        # Check if total spendings has not reached threshold
         if spendings == max_spendings:
             break
-        # check next action will not go over the max spending cap
+        # Check next action will not go over threshold
         elif (spendings + action[1]) <= max_spendings:
             spendings += action[1]
-            earnings += action[1] * action[2] / 100
+            earnings += action[1] * action[2]
             actions_selected.append(action)
 
-    # Show results:
-    print(f"Total spendings : {spendings}")
-    print(f"Total earnings : {earnings}")
-    print("Actions selected :")
-    for action in actions_selected:
-        print(action[0])
+    return spendings, earnings, actions_selected
 
 
 def main():
     # Initiate chronometer
     start = time.time()
 
-    # List of given actions [name, price, percentage]
-    actions = [
-        ["action_01", 20, 5],
-        ["action_02", 30, 10],
-        ["action_03", 50, 15],
-        ["action_04", 70, 20],
-        ["action_05", 60, 17],
-        ["action_06", 80, 25],
-        ["action_07", 22, 7],
-        ["action_08", 26, 11],
-        ["action_09", 48, 13],
-        ["action_10", 34, 27],
-        ["action_11", 42, 17],
-        ["action_12", 110, 9],
-        ["action_13", 38, 23],
-        ["action_14", 14, 1],
-        ["action_15", 18, 3],
-        ["action_16", 8, 8],
-        ["action_17", 4, 12],
-        ["action_18", 10, 14],
-        ["action_19", 24, 21],
-        ["action_20", 114, 18],
-    ]
-    max_spending = 500
-
-    greedy(actions, max_spending)
+    result = greedy(ACTIONS, BUDGET)
+    print(f"--- Greedy algorithm (20 actions) ---\n \
+    Total costs : {result[0]}\n \
+    Maximum total profit : {result[1]}\n \
+    Selected actions: {result[2]}")
 
     # Calculate runtime and display
+    end = time.time()
+    delta_time = end - start
+    print(f"\n ## The program runs in {delta_time} seconds ##\n")
+
+    start = time.time()
+
+    result2 = greedy(DATASET1, BUDGET)
+    print(f"--- Greedy algorithm (DATASET1) ---\n \
+    Total costs : {result2[0]}\n \
+    Maximum total profit : {result2[1]}\n \
+    Selected actions: {result2[2]}")
+
+    end = time.time()
+    delta_time = end - start
+    print(f"\n ## The program runs in {delta_time} seconds ##\n")
+
+    start = time.time()
+
+    result3 = greedy(DATASET2, BUDGET)
+    print(f"--- Greedy algorithm (DATASET2) ---\n \
+    Total costs : {result3[0]}\n \
+    Maximum total profit : {result3[1]}\n \
+    Selected actions: {result3[2]}")
+
     end = time.time()
     delta_time = end - start
     print(f"\n ## The program runs in {delta_time} seconds ##\n")
